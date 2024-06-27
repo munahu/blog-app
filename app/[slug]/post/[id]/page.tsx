e6516952e1567a1eb2ee7c589e548402817609b3
Muna Hussen <muna.hussen@hotmail.ca>
2024-06-27 13:06:28 -0400

    feat: navigate between posts on selected post page

--- app/[slug]/post/[id]/page.tsx
+++ app/[slug]/post/[id]/page.tsx
@@ -1,7 +1,8 @@
-import { getPost } from "@/app/actions";
+import { getPost, getPosts } from "@/app/actions";
 import { notFound } from "next/navigation";
 import FullPost from "@/app/components/FullPost";
 import Layout from "@/app/components/Layout";
+import { Category } from "@prisma/client";
 
 export default async function Page({
   params,
@@ -9,13 +10,17 @@ export default async function Page({
   params: { slug: string; id: string };
 }) {
   const postId = Number(params.id);
-
   const post = await getPost(postId);
+  const categoryPosts = await getPosts(params.slug.toUpperCase() as Category);
 
   if (post) {
     return (
       <Layout>
-        <FullPost post={post} categoryURL={params.slug} />
+        <FullPost
+          postId={postId}
+          categoryURL={params.slug}
+          categoryPosts={categoryPosts}
+        />
       </Layout>
     );
   } else {
--- app/components/FullPost.tsx
+++ app/components/FullPost.tsx
@@ -6,19 +6,25 @@ import { useRouter } from "next/navigation";
 import { Prisma } from "@prisma/client";
 
 export default function FullPost({
-  post,
+  postId,
   categoryURL,
+  categoryPosts,
 }: {
-  post: Prisma.PostGetPayload<{ include: { blog: true; author: true } }>;
+  postId: number;
   categoryURL: string;
+  categoryPosts: Prisma.PostGetPayload<{
+    include: { blog: true; author: true };
+  }>[];
 }) {
   const router = useRouter();
-  const { title, description, content, blog } = post;
+  const postIndex = categoryPosts.findIndex((post) => post.id === postId);
+  const { title, description, content, blog, coverImageURL } =
+    categoryPosts[postIndex];
 
   return (
     <div className="relative md:border border-gray rounded-tl-2xl rounded-tr-2xl md:mt-8 mx-4">
       <Image
-        src={post.coverImageURL}
+        src={coverImageURL}
         width={50}
         height={50}
         alt={blog.name}
@@ -49,10 +55,60 @@ export default function FullPost({
           />
         </div>
       </div>
+      <div className="hidden absolute left-10 lg:flex flex-col">
+        <button
+          className="mb-7 group"
+          onClick={() =>
+            router.push(
+              `/${categoryURL}/post/${categoryPosts[postIndex - 1].id}`
+            )
+          }
+          disabled={postIndex === 0}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={0.5}
+            stroke="currentColor"
+            className="size-6 md:size-10 cursor-pointer group-disabled:stroke-neutral-500 group-disabled:cursor-not-allowed"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </button>
+        <button
+          className="mb-7 group"
+          onClick={() =>
+            router.push(
+              `/${categoryURL}/post/${categoryPosts[postIndex + 1].id}`
+            )
+          }
+          disabled={postIndex === categoryPosts.length - 1}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={0.5}
+            stroke="currentColor"
+            className="size-6 md:size-10 cursor-pointer group-disabled:stroke-neutral-500 group-disabled:cursor-not-allowed"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </button>
+      </div>
       <div className="md:px-12 lg:max-w-[728px] lg:m-auto">
-        <div className="pb-8 border-b border-[#464a5b] ">
+        <div className="pb-8 border-b border-[#464a5b]">
           <p className="uppercase text-xs md:text-sm mb-5">{blog.name}</p>
-          <p className="text-3xl font-semibold mb-3">{title}</p>
+          <p className="text-3xl md:text-4xl font-semibold mb-3">{title}</p>
           <p className="text-lg opacity-70 font-light">{description}</p>
         </div>
         {content.map((item, index) => (
